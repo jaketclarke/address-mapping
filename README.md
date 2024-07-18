773da0a359babf8a19aead3c0dc4cb48b55a0b49
Jake Clarke <jake.clarke@secnewgate.com.au>
2024-07-18 13:55:51 +1000

    proof of concept hiding API key, & logging click on marker to console

---
+++ README.md
@@ -0,0 +1,9 @@
+# Address Geocoder
+
+Project to build a useful set of information from an address.
+
+## Setup
+
+* create a .env file, and add a key for opencagegeocoding, with the name `OPENCAGE_GEOCODING_API_KEY`
+* npm install
+* npm run dev
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv');
+const ejs = require('ejs');
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+  res.render('index', {
+    mapsApiKey: process.env.OPENCAGE_GEOCODING_API_KEY
+  });
 });
 
 app.listen(PORT, () => {
